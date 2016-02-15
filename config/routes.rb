560f2b86bb2b4afae905cf50439a0bbefaf49dcf
nasrinfa <fathollahi.nasrin@yahoo.com>
2016-02-15 20:58:38 +0330

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'static_pages#home'
+  get  'static_pages/help'
+  get  'static_pages/about'
 end
